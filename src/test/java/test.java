90ec72dbd488c0659e1e55e33f55583e35586f0f
Esatgs <estgke73@gmail.com>
2024-10-30 18:05:32 -0400

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is the project version 2");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-
+        System.out.println("This is the project version 3");
+        S
     }
 }
-//done with version two
+//done with version three
